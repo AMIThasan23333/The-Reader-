4260a4d00050fb001a282502e5e579e35a08d0d5
My Name <cpgsom819@gmail.com>
2023-04-10 22:48:40 +0800

    home router added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
 import React from 'react'
 import Header from './components/Header'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './components/Home';
+
+
+const router = createBrowserRouter([
+
+
+  {
+
+    path : '/',
+
+    element: <Home></Home>
+  },
+
+  {
+
+    path : '/home',
+
+    element: <Home></Home>
+  }
+
+
+])
 
 function App() {
-  return  <Header></Header>
+  return (
+
+    <div>
+       <Header></Header>
+
+       <RouterProvider router={router}></RouterProvider>
+    </div>
+
+
+  )
 }
 
 export default App
